bbafc301ab9b895070a9770d066d124055c4da36
evancodez <milenkoevan@gmail.com>
2023-12-05 02:53:22 -0500

    tuple to Cube Coord

--- src/cs3500/reversi/controller/ReversiAdapter.java
+++ src/cs3500/reversi/controller/ReversiAdapter.java
@@ -1,6 +1,9 @@
 package cs3500.reversi.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cs3500.reversi.model.BasicReversi;
 import cs3500.reversi.model.Tuple;
@@ -20,16 +23,69 @@ public class ReversiAdapter implements PlayerFeatures {
 
   @Override
   public void makeMove(Tuple<Integer, Integer> coordinate) {
-    List<Tuple<Integer, Integer>> coordinates = model.getCoordinates();
-    BoardPosn boardPosn = convertRowColtoBoardPosn(coordinates);
+    BoardPosn boardPosn = tupleToBoardPosn(coordinate);
     providerController.moveMade(boardPosn);
 
   }
 
-  private BoardPosn convertRowColtoBoardPosn(List<Tuple<Integer, Integer>> coordinates) {
-    //
-    return null;
+  private BoardPosn tupleToBoardPosn(Tuple<Integer, Integer> coordinate) {
+    Map<Tuple<Integer, Integer>, BoardPosn> hexGridMap = convertRowColBoardtoCubeCoordBoard();
+    return hexGridMap.get(coordinate);
+  }
+
+  private Map<Tuple<Integer, Integer>, BoardPosn> convertRowColBoardtoCubeCoordBoard() {
+    int diameter = model.getNumRows();
+    List<Tuple<Integer, Integer>> rowColList = generateRowColCoordinates(diameter);
+    List<BoardPosn> cubeList = generateCubeCoordinates(diameter);
+
+    Map<Tuple<Integer, Integer>, BoardPosn> hexGridMap = new HashMap<>();
+    for (int i = 0; i < rowColList.size(); i++) {
+      hexGridMap.put(rowColList.get(i), cubeList.get(i));
+    }
+
+    return hexGridMap;
+  }
+
+  private static List<Tuple<Integer, Integer>> generateRowColCoordinates(int diameter) {
+    List<Tuple<Integer, Integer>> rowColCoordinates = new ArrayList<>();
+    int radius = (diameter - 1) / 2;
+
+    for (int row = 0; row < diameter; row++) {
+      int cols = diameter - Math.abs(row - radius);
+      for (int col = 0; col < cols; col++) {
+        int adjustedCol;
+        if (row <= radius) {
+          adjustedCol = col;
+        } else {
+          adjustedCol = col + (row - radius);
+        }
+        rowColCoordinates.add(new Tuple<>(row, adjustedCol));
+      }
+    }
+
+    return rowColCoordinates;
+  }
+
+  private static List<BoardPosn> generateCubeCoordinates(int diameter) {
+    List<BoardPosn> cubeCoordinates = new ArrayList<>();
+    int radius = (diameter - 1) / 2;
+
+    for (int r = -radius; r <= radius; r++) {
+      for (int q = -radius; q <= radius; q++) {
+        int s = -r - q;
+        if (Math.abs(s) <= radius) {
+          cubeCoordinates.add(new BoardPosn(q, r, s));
+        }
+      }
+    }
+
+    return cubeCoordinates;
   }
 
 
+
+
+
 }
+
+
