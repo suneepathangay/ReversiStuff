5fa6674642f62108f0274eccebdba0c3f090836d
suneetpathangay <suneetpathangay@gmail.com>
2023-12-05 02:01:55 -0500

    "started model adapter"

--- src/cs3500/reversi/model/ModelAdapter.java
+++ src/cs3500/reversi/model/ModelAdapter.java
@@ -1,6 +1,7 @@
 package cs3500.reversi.model;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cs3500.reversi.provider.model.BoardPosn;
@@ -12,6 +13,8 @@ import cs3500.reversi.provider.model.ReadOnlyReversiModel;
 import cs3500.reversi.provider.model.ReversiModel;
 
 
+//TODO figure out how to convert thier coordiantes to my coordinaes
+
 //readonlyReversi their model
 public class ModelAdapter implements ReadOnlyReversiModel {
 
@@ -22,6 +25,14 @@ public class ModelAdapter implements ReadOnlyReversiModel {
     this.model=model;
   }
 
+  //converts their coordinates to mine
+  private Tuple<Integer,Integer> convertProviderCoord(BoardPosn coor){
+
+
+
+
+    return null;
+  }
 
   @Override
   public int getNumCapturedHexagons(PlayerColor playerColor) throws IllegalArgumentException {
@@ -49,16 +60,43 @@ public class ModelAdapter implements ReadOnlyReversiModel {
   @Override
   public List<BoardPosn> getCapturedHexagons(PlayerColor playerColor) throws IllegalArgumentException {
     //
+    Colors color;
+    if(playerColor==PlayerColor.BLACK){
+      color=Colors.BLACK;
+    }else{
+      color=Colors.WHITE;
+    }
+
+
+    List<Tuple<Integer,Integer>> pieces=new ArrayList<>();
+    for(int row=0; row< model.getNumRows(); row++){
+      List<IDisc> currRow=model.getCurrBoard().get(row);
+      for(int col=0; col<currRow.size(); col++){
+        IDisc disc=currRow.get(col);
+        if(disc.getColor()==color) {
+          int x = disc.getRow();
+          int y = disc.getCol();
+          Tuple<Integer, Integer> coordinate = new Tuple<>(x, y);
+          pieces.add(coordinate);
+        }
+      }
+    }
+    //convert my coordinate to their coordinate system
+
 
     return null;
   }
 
   @Override
   public PlayerColor getPlayerTurn() throws IllegalStateException {
+    PlayerColor color;
+    if(model.getTurn()==Colors.BLACK){
+      color=PlayerColor.BLACK;
+    }else{
+      color=PlayerColor.WHITE;
+    }
 
-
-
-    return null;
+    return color;
   }
 
   @Override
@@ -68,12 +106,24 @@ public class ModelAdapter implements ReadOnlyReversiModel {
 
   @Override
   public GameStatus getGameStatus() {
+
+
+
     return null;
   }
 
   @Override
   public PlayerColor getWinner() {
-    return null;
+    PlayerColor winner;
+    Tuple<Integer,Integer> scores=model.getScore();
+    if(scores.getFirst()> scores.getSecond()){
+      winner=PlayerColor.WHITE;
+    } else if (scores.getFirst()<scores.getSecond()) {
+      winner=PlayerColor.BLACK;
+    }else{
+      throw new IllegalStateException("no winner");
+    }
+    return winner;
   }
 
   @Override
@@ -98,9 +148,10 @@ public class ModelAdapter implements ReadOnlyReversiModel {
 
   @Override
   public int getBoardSize() {
-    return 0;
+    return model.getLength();
   }
 
+  //TODO
   @Override
   public IBoard getBoard(){
     return null;
